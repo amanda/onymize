05843d5f7dfb9f16f9f4de0e248dd8b4aabe8808
amanda <pickering.amanda@gmail.com>
2016-06-28 19:33:41 -0400

    working on antonyms

--- onymize.py
+++ onymize.py
@@ -5,22 +5,35 @@ from nltk.corpus import wordnet as wn
 from nltk import word_tokenize, pos_tag
 
 
-def synonymize(text):
+def find_syns(tokenized_text):
     '''(str) -> {adjective: [synonyms]}'''
-    tagged = pos_tag(word_tokenize(text))
+    tagged = pos_tag(tokenized_text)
+    synonyms = {}
     for t in tagged:
         if t[1] == 'JJ' and wn.synsets(t[0]):
             lemmas = set([s.lemma_names()[0] for s in wn.synsets(t[0])])
             lemmas.remove(t[0])
-            return {t[0]: list(lemmas)}
+            synonyms[t[0]] = list(lemmas)
+    return synonyms
+
+def find_ants(tokenized_text):
+    '''(str) -> {adjective: [antonyms]}'''
+    antonyms = {}
+    tagged = pos_tag(tokenized_text)
+    # TODO: clean up below?
+    for t in tagged:
+        if t[1] == 'JJ' and wn.synsets(t[0]):
+            for syn in wn.synsets(t[0]):
+                for l in syn.lemmas():
+                    if l.antonyms():
+                        antonyms[t[0]] = l.antonyms()[0].name() # TODO check if only 1
+    return antonyms
+
 
 def antonymize(text):
-    '''(str) -> {adjective: [antonyms]}'''
-    pass
-
-
-def replace_words(textfile, onymize_desire):
-    '''iterates through text in a file, writes
-    new synonym or antonym over each adjective'''
-    pass
-    pass
+    '''iterates through text and writes an
+    antonym over each adjective...it's slow'''
+    tokenized = word_tokenize(text)
+    ants = find_ants(tokenized)
+    for w in tokenized:
+        pass
