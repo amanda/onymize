4d126ab4c680e79a531b16fd6c33fe731478ae59
amanda <pickering.amanda@gmail.com>
2017-04-18 16:39:08 -0400

    [wip] make text sad

--- negativity.py
+++ negativity.py
@@ -4,3 +4,13 @@ if postive, antonymize
 '''
 
 from textblob import TextBlob
+from onymize import onymize
+
+def get_setiment(text):
+    blob = TextBlob(text)
+    return blob.sentiment.polarity
+
+def make_it_sad(text, sadness_cutoff=1):
+    sentiment = get_setiment(text):
+    if sentiment < sadness_cutoff:
+        return onymize(text, False)
